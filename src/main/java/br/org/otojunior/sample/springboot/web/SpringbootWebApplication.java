1af61b908a514c13bb257b6358a839e0defbf26a
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2018-07-26 17:03:13 -0300

    Commit inicial

---
+++ src/main/java/br/org/otojunior/sample/springboot/web/SpringbootWebApplication.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ */
+package br.org.otojunior.sample.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Oto Soares Coelho Junior (oto.coelho-junior@serpro.gov.br)
+ *
+ */
+@SpringBootApplication
+public class SpringbootWebApplication {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebApplication.class, args);
+	}
+}
